24793cb82b28272a5dcd5d08e8df17ffae7c4cb8
Ngin Hak <n-hak@beniten.com>
2021-11-05 10:31:17 +0700

    fix category image

--- backend/apps/categories/serializers.py
+++ backend/apps/categories/serializers.py
@@ -1,8 +1,9 @@
+from rest_framework.fields import ImageField
 from .models import Category
 from rest_framework import serializers
 
-
 class CategorySerializer(serializers.ModelSerializer):
+    image = ImageField(read_only = True)
     class Meta:
         model = Category
         fields = '__all__'
