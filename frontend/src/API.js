709641e579957627f2fca8000d40c4b53f029931
panhavoan-leng <l-sovanpanhavoan@beniten.com>
2021-11-12 14:11:51 +0700

    add dot env

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,12 +1,14 @@
 import axios from "axios";
+require('dotenv').config()
 
 export const LOGIN_USER_KEY = "HIVE_TECHWEAR_LOGIN_USER_KEY";
+const { REACT_APP_ENVIRONMENT, REACT_APP_API_BASE_URL_PROD, REACT_APP_API_BASE_URL_DEV } = process.env;
+let baseURL;
 
-var baseURL;
-if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === "PRODUCTION") {
-	baseURL = process.env.REACT_APP_API_BASE_URL;
+if (REACT_APP_ENVIRONMENT === "PRODUCTION") {
+	baseURL = REACT_APP_API_BASE_URL_PROD;
 } else {
-	baseURL = "http://127.0.0.1:8000";
+	baseURL = REACT_APP_API_BASE_URL_DEV;
 }
 
 const api = axios.create({
